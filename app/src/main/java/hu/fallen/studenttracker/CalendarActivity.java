9d21baf28b4c65da22ba8f01736fff8ff554d778
arsyee <2476948+arsyee@users.noreply.github.com>
2018-05-03 09:30:18 +0200

    removed custom toolbar from CalendarActivity
    it was hiding the base toolbar

--- app/src/main/java/hu/fallen/studenttracker/CalendarActivity.java
+++ app/src/main/java/hu/fallen/studenttracker/CalendarActivity.java
@@ -13,8 +13,6 @@ public class CalendarActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
