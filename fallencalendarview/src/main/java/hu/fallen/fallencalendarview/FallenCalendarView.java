fc2fca1ba9e2854cc5e9527fb2cf673a53e15b58
arsyee <2476948+arsyee@users.noreply.github.com>
2018-05-04 17:36:47 +0200

    First implementation of left/right swipe to modify date

--- fallencalendarview/src/main/java/hu/fallen/fallencalendarview/FallenCalendarView.java
+++ fallencalendarview/src/main/java/hu/fallen/fallencalendarview/FallenCalendarView.java
@@ -6,23 +6,24 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.widget.TextView;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import timber.log.Timber;
 
-public class FallenCalendarView extends ConstraintLayout implements ScaleGestureDetector.OnScaleGestureListener {
+public class FallenCalendarView extends ConstraintLayout
+        implements ScaleGestureDetector.OnScaleGestureListener, GestureDetector.OnGestureListener {
 
     private final ScaleGestureDetector mScaleDetector;
+    private final GestureDetector mGestureDetector;
+
     private ViewLevel viewLevel = ViewLevel.day;
 
     @BindView(R2.id.debug) TextView tvDebugView;
@@ -62,6 +63,7 @@ public class FallenCalendarView extends ConstraintLayout implements ScaleGesture
         mCalendar = Calendar.getInstance();
 
         mScaleDetector = new ScaleGestureDetector(context, this);
+        mGestureDetector = new GestureDetector(context, this);
         onChanged();
     }
 
@@ -79,9 +81,14 @@ public class FallenCalendarView extends ConstraintLayout implements ScaleGesture
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        mScaleDetector.onTouchEvent(event);
-        return true;
+        boolean result = mScaleDetector.onTouchEvent(event);
+        if (!mScaleDetector.isInProgress()) {
+            result = mGestureDetector.onTouchEvent(event);
+        }
+        return result || super.onTouchEvent(event);
     }
+
+    // Implementation of ScaleGestureDetector.OnScaleGestureListener
 
     @Override
     public boolean onScale(ScaleGestureDetector detector) {
@@ -104,6 +111,101 @@ public class FallenCalendarView extends ConstraintLayout implements ScaleGesture
             viewLevel = viewLevel.prev();
         }
         onChanged();
+    }
+
+    // Implementation of GestureDetector.OnGestureListener
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        Timber.d("onFling: %f, %f, %s, %s", velocityX, velocityY, e1, e2);
+
+        final int SWIPE_THRESHOLD = 100;
+        final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+        try {
+            float diffY = e2.getY() - e1.getY();
+            float diffX = e2.getX() - e1.getX();
+            if (Math.abs(diffX) > Math.abs(diffY)) {
+                if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffX > 0) {
+                        return onSwipeRight();
+                    } else {
+                        return onSwipeLeft();
+                    }
+                }
+            }
+            else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                if (diffY > 0) {
+                    return onSwipeBottom();
+                } else {
+                    return onSwipeTop();
+                }
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return false;
+    }
+
+    private boolean onSwipeRight() {
+        mCalendar.add(getCalendarField(viewLevel), 1);
+        onChanged();
+        return true;
+    }
+
+    private boolean onSwipeLeft() {
+        mCalendar.add(getCalendarField(viewLevel), -1);
+        onChanged();
+        return true;
+    }
+
+    private boolean onSwipeBottom() {
+        return false;
+    }
+
+    private boolean onSwipeTop() {
+        return false;
+    }
+
+    private int getCalendarField(ViewLevel viewLevel) {
+        switch (viewLevel) {
+            case yearList:
+                return Calendar.YEAR;
+            case year:
+                return Calendar.YEAR;
+            case month:
+                return Calendar.MONTH;
+            case week:
+                return Calendar.WEEK_OF_YEAR;
+            case day:
+                return Calendar.DAY_OF_MONTH;
+        }
+        return Calendar.DAY_OF_MONTH;
     }
 
     private enum ViewLevel {
