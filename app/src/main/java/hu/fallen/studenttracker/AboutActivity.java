ae37a06a5b3e72c739905a19b5b48032c8d1b7bd
arsyee <2476948+arsyee@users.noreply.github.com>
2018-07-05 23:20:34 +0200

    added some functionality to log and clear preferences

--- app/src/main/java/hu/fallen/studenttracker/AboutActivity.java
+++ app/src/main/java/hu/fallen/studenttracker/AboutActivity.java
@@ -1,6 +1,12 @@
 package hu.fallen.studenttracker;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+
+import java.util.Map;
+
+import timber.log.Timber;
 
 public class AboutActivity extends BaseActivity {
 
@@ -8,5 +14,21 @@ public class AboutActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        printPreferences();
+    }
+
+    public void clearPreferences(View view) {
+        Timber.d("Clearing preferences");
+        PreferenceManager.getDefaultSharedPreferences(this).edit().clear().apply();
+        printPreferences();
+    }
+
+    private void printPreferences() {
+        Timber.d("Preference list:");
+        Map<String, ?> prefs = PreferenceManager.getDefaultSharedPreferences(this).getAll();
+        for(Map.Entry<String,?> entry : prefs.entrySet()) {
+            Timber.d("map values: %s - %s",entry.getKey(),  entry.getValue());
+        }
     }
 }
