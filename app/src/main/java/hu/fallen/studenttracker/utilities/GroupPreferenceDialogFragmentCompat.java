1e547e45bbe0692d9dad83ca44a2b3fd9f5847f3
arsyee <2476948+arsyee@users.noreply.github.com>
2018-07-06 01:55:40 +0200

    new group can be created on group selection dialog

--- app/src/main/java/hu/fallen/studenttracker/utilities/GroupPreferenceDialogFragmentCompat.java
+++ app/src/main/java/hu/fallen/studenttracker/utilities/GroupPreferenceDialogFragmentCompat.java
@@ -1,16 +1,32 @@
 package hu.fallen.studenttracker.utilities;
 
+import android.Manifest;
+import android.app.LoaderManager;
+import android.content.AsyncTaskLoader;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Loader;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.preference.DialogPreference;
 import android.support.v7.preference.PreferenceDialogFragmentCompat;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import hu.fallen.studenttracker.R;
 import timber.log.Timber;
 
-public class GroupPreferenceDialogFragmentCompat extends PreferenceDialogFragmentCompat {
+public class GroupPreferenceDialogFragmentCompat extends PreferenceDialogFragmentCompat
+                                                implements LoaderManager.LoaderCallbacks<String> {
+    private static final int PERMISSION_REQUEST_WRITE_CONTACTS_ID = 273;
     private static Group[] mGroups;
     private View mView = null;
 
@@ -46,7 +62,7 @@ public class GroupPreferenceDialogFragmentCompat extends PreferenceDialogFragmen
     }
 
     @Override
-    protected void onBindDialogView(View view) {
+    protected void onBindDialogView(final View view) {
         super.onBindDialogView(view);
         mView = view;
         RadioGroup studentSelector = view.findViewById(R.id.student_selector);
@@ -73,6 +89,71 @@ public class GroupPreferenceDialogFragmentCompat extends PreferenceDialogFragmen
                 }
             }
         }
+
+        final Button createNewGroup = view.findViewById(R.id.create_new_group);
+        final LinearLayout newLayout = view.findViewById(R.id.new_layout);
+        final EditText newGroupName = view.findViewById(R.id.new_group_name);
+        final Button newGroupOk = view.findViewById(R.id.new_group_ok);
+        createNewGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newLayout.setVisibility(View.VISIBLE);
+                createNewGroup.setVisibility(View.GONE);
+            }
+        });
+        newGroupOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNewGroup.setVisibility(View.VISIBLE);
+                newLayout.setVisibility(View.GONE);
+                Timber.d("OK Clicked: %s", newGroupName.getText());
+                if (newGroupName.getText().toString().length() > 0) {
+                    Timber.d("So we create a Loader...");
+                    Bundle args = new Bundle();
+                    args.putString("newGroup", newGroupName.getText().toString());
+                    if (getActivity().getLoaderManager().getLoader(1) != null && getActivity().getLoaderManager().getLoader(1).isStarted()) {
+                        Timber.d("Loader is already running...");
+                        getActivity().getLoaderManager().destroyLoader(1);
+                    }
+                    getActivity().getLoaderManager().initLoader(1, args, GroupPreferenceDialogFragmentCompat.this).forceLoad();
+                }
+            }
+        });
+    }
+
+    @Override
+    public Loader<String> onCreateLoader(int id, final Bundle args) {
+        Timber.d("This is where the Loader is created...");
+        if (ContextCompat.checkSelfPermission(this.getContext(), Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            Timber.d("Permission denied...");
+            ActivityCompat.requestPermissions(this.getActivity(), new String[]{Manifest.permission.WRITE_CONTACTS}, PERMISSION_REQUEST_WRITE_CONTACTS_ID);
+            return null;
+        } else {
+            return new AsyncTaskLoader<String>(this.getContext()) {
+                @Override
+                public String loadInBackground() {
+                    Timber.d("loadInBackground called with %s", args.getString("newGroup"));
+                    ContentValues values = new ContentValues();
+                    values.put(ContactsContract.Groups.TITLE, args.getString("newGroup"));
+                    Uri uri = GroupPreferenceDialogFragmentCompat.this.getActivity().getContentResolver().insert(
+                            ContactsContract.Groups.CONTENT_URI,
+                            values
+                    );
+                    Timber.d("Insert returned with %s", uri.toString());
+                    return Long.toString(ContentUris.parseId(uri));
+                }
+            };
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+        Timber.d("Group added: %s", data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+        // pass
     }
 
     public static class Group {
