79016753ab3cac92287c46531f44727bb798db4b
arsyee <2476948+arsyee@users.noreply.github.com>
2018-07-07 17:02:56 +0200

    LiveData is now responsible for loading it's own data

--- app/src/main/java/hu/fallen/studenttracker/model/StudentModel.java
+++ app/src/main/java/hu/fallen/studenttracker/model/StudentModel.java
@@ -5,9 +5,9 @@ import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.content.Context;
 import android.database.Cursor;
 import android.os.AsyncTask;
-import android.os.Looper;
 import android.preference.PreferenceManager;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
@@ -15,51 +15,57 @@ import android.support.annotation.NonNull;
 import timber.log.Timber;
 
 public class StudentModel extends AndroidViewModel {
-    private MutableLiveData<Cursor> students;
+    private StudentsLiveCursor students;
 
     public StudentModel(@NonNull Application application) {
         super(application);
+        students = new StudentsLiveCursor(application);
     }
 
     public LiveData<Cursor> getStudents() {
-        if (students == null) {
-            students = new MutableLiveData<>();
-            loadStudents();
-        }
         return students;
     }
 
-    @SuppressLint("StaticFieldLeak")
-    private void loadStudents() {
-        new AsyncTask<Void, Void, Cursor>() {
-            @Override
-            protected Cursor doInBackground(Void... voids) {
-                String[] PROJECTION = {
-                        ContactsContract.Data._ID,
-                        ContactsContract.Data.LOOKUP_KEY,
-                        ContactsContract.Data.DISPLAY_NAME_PRIMARY,
-                        ContactsContract.Data.CONTACT_ID,
-                        ContactsContract.Data.DATA1
-                };
-                String SELECTION = ContactsContract.Data.DATA1 + " LIKE ?";
-                String group = PreferenceManager.getDefaultSharedPreferences(getApplication()).getString("group", null);
-                Timber.d("Querying group %s", group);
-                if (group == null) {
-                    return null;
-                }
-                String[] selectionArgs = { group };
-                return getApplication().getContentResolver().query(
-                        ContactsContract.Data.CONTENT_URI,
-                        PROJECTION,
-                        SELECTION,
-                        selectionArgs,
-                        null);
-            }
+    private static class StudentsLiveCursor extends MutableLiveData<Cursor> {
+        private final Context context;
 
-            @Override
-            protected void onPostExecute(Cursor cursor) {
-                students.setValue(cursor);
-            }
-        }.execute();
+        StudentsLiveCursor(Context context) {
+            this.context = context;
+            loadStudents();
+        }
+
+        @SuppressLint("StaticFieldLeak")
+        private void loadStudents() {
+            new AsyncTask<Void, Void, Cursor>() {
+                @Override
+                protected Cursor doInBackground(Void... voids) {
+                    String[] PROJECTION = {
+                            ContactsContract.Data._ID,
+                            ContactsContract.Data.LOOKUP_KEY,
+                            ContactsContract.Data.DISPLAY_NAME_PRIMARY,
+                            ContactsContract.Data.CONTACT_ID,
+                            ContactsContract.Data.DATA1
+                    };
+                    String SELECTION = ContactsContract.Data.DATA1 + " LIKE ?";
+                    String group = PreferenceManager.getDefaultSharedPreferences(context).getString("group", null);
+                    Timber.d("Querying group %s", group);
+                    if (group == null) {
+                        return null;
+                    }
+                    String[] selectionArgs = { group };
+                    return context.getContentResolver().query(
+                            ContactsContract.Data.CONTENT_URI,
+                            PROJECTION,
+                            SELECTION,
+                            selectionArgs,
+                            null);
+                }
+
+                @Override
+                protected void onPostExecute(Cursor cursor) {
+                    setValue(cursor);
+                }
+            }.execute();
+        }
     }
 }
