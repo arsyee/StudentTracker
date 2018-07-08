5911289305f889a73645842ad5036e1d71bfa7a3
arsyee <2476948+arsyee@users.noreply.github.com>
2018-07-08 12:45:28 +0200

    sample update implementation in AboutActivity

--- app/src/main/java/hu/fallen/studenttracker/AboutActivity.java
+++ app/src/main/java/hu/fallen/studenttracker/AboutActivity.java
@@ -1,14 +1,20 @@
 package hu.fallen.studenttracker;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 
+import java.util.List;
 import java.util.Map;
 
+import hu.fallen.studenttracker.model.Student;
+import hu.fallen.studenttracker.model.StudentModel;
 import timber.log.Timber;
 
 public class AboutActivity extends BaseActivity {
+
+    StudentModel model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,7 @@ public class AboutActivity extends BaseActivity {
         setContentView(R.layout.activity_about);
 
         printPreferences();
+        model = ViewModelProviders.of(this).get(StudentModel.class);
     }
 
     public void clearPreferences(View view) {
@@ -29,6 +36,16 @@ public class AboutActivity extends BaseActivity {
         Map<String, ?> prefs = PreferenceManager.getDefaultSharedPreferences(this).getAll();
         for(Map.Entry<String,?> entry : prefs.entrySet()) {
             Timber.d("map values: %s - %s",entry.getKey(),  entry.getValue());
+        }
+    }
+
+    public void updateStudents(View view) {
+        List<Student> students = model.getStudents().getValue();
+        if (students == null) return;
+        for (Student student : students) {
+            // TODO: I think this is not thread-safe, but won't be part of the final app anyway
+            student.setStatus(Student.STATUS.ACTIVE);
+            model.getStudents().update(student);
         }
     }
 }
